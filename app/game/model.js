9c3dd930a9a5c01c429cb52496287a3a2a7756df
frere921 <cook@smile.org>
2018-12-14 09:43:30 +0800

    the model implement

--- app/game/model.js
+++ app/game/model.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    templatePicture: DS.belongsTo('picture',{ async: false }),
+    gamePicture: DS.belongsTo('picture',{ async: false }),
+    init(){
+        this._super(...arguments);
+        this.set('templatePicture',this.store.createRecord('picture'));
+        this.set('gamePicture',this.store.createRecord('picture'));
+    }
 });
--- app/picture/model.js
+++ app/picture/model.js
@@ -1,5 +1,22 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  cells: DS.hasMany('cell',{ async: false }),
 
+  init(){
+    this._super(...arguments);
+    for(let i =0 ;i<100;i++){
+      this.cells.createRecord()
+    }
+  },
+  load(template){
+
+  },
+  clear(){
+
+  },
+  // serialized:
+  asJson(){
+    
+  }
 });
